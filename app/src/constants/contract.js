95e234f8f0badd39ad6f4a38447e646a4d4092a5
QuangHuy10 <dqhuy.20it7@vku.udn.vn>
2024-05-30 10:42:04 +0700

    first commit

---
+++ app/src/constants/contract.js
@@ -0,0 +1,4 @@
+export const CHAIN_ID = 80001;
+export const RPC_URL = 'https://polygon-mumbai.g.alchemy.com/v2/zCgmIL8j7_fMuq2Q8uDQtcuj_w4tne2E';
+
+export const MARKETPLACE_ADDRESS = '0x35813c9ac863002925f884ce967542a89635248d';
---
+++ app/src/constants/tab-name.js
@@ -0,0 +1,88 @@
+export const FOOTER_MENU = [
+  {
+    title: 'AvengersFI',
+    items: [
+      {
+        title: 'Privacy Policy',
+        link: '#',
+      },
+      {
+        title: 'Terms & Use',
+        link: '#',
+      },
+      {
+        title: 'Terms & Conditions',
+        link: '#',
+      },
+      {
+        title: 'Github',
+        link: '#',
+      },
+      {
+        title: 'Contract Us',
+        link: '#',
+      },
+    ],
+  },
+  {
+    title: 'Trade',
+    items: [
+      {
+        title: 'Marketplace',
+        link: '/marketplace/nfts',
+      },
+      {
+        title: 'Sell NFT',
+        link: '/marketplace/sell',
+      },
+    ],
+  },
+  {
+    title: 'Community & Help',
+    items: [
+      {
+        title: 'Discord',
+        link: '#',
+      },
+      {
+        title: 'Telegram',
+        link: '#',
+      },
+      {
+        title: 'FAQ',
+        link: '#',
+      },
+    ],
+  },
+];
+
+export const HEADER_MENU = [
+  {
+    title: 'Trade',
+    items: [
+      {
+        title: 'Marketplace',
+        link: '/marketplace/nfts',
+      },
+      {
+        title: 'Sell NFT',
+        link: '/marketplace/sell',
+      },
+    ],
+  },
+];
+
+export const MARKETPLACE_TABS = [
+  {
+    text: 'Marketplace',
+    url: '/marketplace/nfts',
+  },
+  {
+    text: 'Your selling NFTs',
+    url: '/marketplace/selling-nfts',
+  },
+  {
+    text: 'Sell your NFT',
+    url: '/marketplace/sell',
+  },
+];
